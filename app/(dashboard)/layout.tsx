425c53c5a19abc10ea594fb1ce9b15b37a7ea80c
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-08 19:16:07 +0530

    feat: restructure dashboard layout and user pages with new components

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function layout({children}: {children: React.ReactNode}) {
+    return (
+        <div>
+            <h1>dashboard layout</h1>
+            {children}
+        </div>
+    )
+}
+
+export default layout;
