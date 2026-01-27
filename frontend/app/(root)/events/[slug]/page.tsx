6bed476608b84169fb9ff2d7ac7f49e932fd4a74
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-27 18:04:41 +0530

    folder change

--- frontend/app/(root)/events/[slug]/page.tsx
+++ frontend/app/(root)/events/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { eventsApi } from "@/lib/api/events";
 import { cacheLife } from "next/cache";
 import React, { Suspense } from "react";
 import EventDetails from "./EventDetails";
-
+"use cache"
 async function Page({ params }: { params: Promise<{ slug: string }> }) {
     // 'use cache'
     // cacheLife("hours")
