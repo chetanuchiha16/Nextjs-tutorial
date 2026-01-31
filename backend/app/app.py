ce7d6f2ff820402efb169dadab69adea5ff70eaa
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-31 13:15:54 +0530

    added lifespan

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,17 @@
+from contextlib import asynccontextmanager
+
+from app.core.database import create_db_and_tables
 from fastapi import FastAPI
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/health")
 def health():
